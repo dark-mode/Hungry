0dd2e0d2438317310e14c3f4537bd3d5cdb45a2c
Ankush Vangari <ankushvangari@gmail.com>
2018-06-28 17:43:07 -0700

    More Modifications

--- restaurant_app/lib/main.dart
+++ restaurant_app/lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_app/HomePage.dart';
 
-void main() => runApp(new HomePage());
+void main() {
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: HomePage(),
+  ));
+}
