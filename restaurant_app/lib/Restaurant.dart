047a5cd01396e4a84d23286afafe48c1cd76944c
Chris Gong <christophergong@berkeley.edu>
2018-06-28 17:36:22 -0700

    restaurant class

---
+++ restaurant_app/lib/Restaurant.dart
@@ -0,0 +1,19 @@
+class Restaurant {
+  double lat, lon, rating;
+  String address, id, icon, name, maps_photo_link;
+  bool open_now;
+  int price_level;
+
+  Restaurant.fromJson(Map<String, dynamic> json) {
+    this.name = json['name'];
+    this.lat =  json['geometry']['location']['lat'];
+    this.lon = json['geometry']['location']['lng'];
+    this.rating = json['rating'];
+    this.address = json['vicinity'];
+    this.id = json['id'];
+    this.icon = json['icon'];
+
+    this.open_now = json['opening_hours']['open_now'];
+    this.price_level = json['price_level'];
+  }
+}
