da4189acedbc4726f8131761846781b9c71078b0
Chris Gong <christophergong@berkeley.edu>
2018-06-28 18:22:25 -0700

    in the process

--- restaurant_app/lib/ResultsPage.dart
+++ restaurant_app/lib/ResultsPage.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
 class ResultsPage extends StatelessWidget {
+  double _lon, _lat;
+  ResultsPage(double lon, double lat) {
+    _lon = lon;
+    _lat = lat;
+  }
+
+  double get lon => _lon;
+  double get lat => _lat;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
