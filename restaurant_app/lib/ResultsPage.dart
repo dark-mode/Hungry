58402e319fdb0fba02014d09ffb8dc7c5d5c2b62
Ankush Vangari <ankushvangari@gmail.com>
2018-06-28 17:48:58 -0700

    Lol oops

--- restaurant_app/lib/ResultsPage.dart
+++ restaurant_app/lib/ResultsPage.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:restaurant_app/HomePage.dart';
 
 class ResultsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('First Screen'),
+        title: Text("Second Screen"),
       ),
       body: Center(
         child: RaisedButton(
-          child: Text('Launch screen'),
           onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => HomePage()),
-            );
+            Navigator.pop(context);
           },
+          child: Text('Go back!'),
         ),
       ),
     );
