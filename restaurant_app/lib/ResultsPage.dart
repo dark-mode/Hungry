855576512aa97e88350a3814812797f37aa3b10f
Ankush Vangari <ankushvangari@gmail.com>
2018-06-28 17:39:01 -0700

    Home and Results Page

---
+++ restaurant_app/lib/ResultsPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            // Navigate back to first screen when tapped!
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
