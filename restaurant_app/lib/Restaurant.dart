bbc86b19106b7be2ddd8455d06ad7cadfaddbac8
Ankush Vangari <ankushvangari@gmail.com>
2018-07-03 17:42:29 -0700

    json tweaking

--- restaurant_app/lib/Restaurant.dart
+++ restaurant_app/lib/Restaurant.dart
@@ -2,8 +2,8 @@ class Restaurant {
   double lat, lon;
   num rating;
   String address, id, icon, name, htmlPhotoLink;
-  //bool open_now;
-  //int price_level;
+  bool open_now;
+  int price_level;
 
   Restaurant.fromJson(Map<String, dynamic> json) {
     this.name = json['name'];
@@ -13,9 +13,9 @@ class Restaurant {
     this.address = json['vicinity'];
     this.id = json['id'];
     this.icon = json['icon'];
-    this.htmlPhotoLink = json['photos'][0]['html_attributions'][0];
+    this.htmlPhotoLink = json['photos'][0]['html_attributions'][0].substring(json['photos'][0]['html_attributions'][0].indexOf("\"") + 1, json['photos'][0]['html_attributions'][0].lastIndexOf("\""));
 
-    //this.open_now = json['opening_hours']['open_now'];
+    this.open_now = json['opening_hours']['open_now'];
     //this.price_level = json['price_level'];
   }
 }
