92dd3f1f3fb45d2edc7f7c559b9f4e78c7c80d10
Chris Gong <christophergong@berkeley.edu>
2018-07-13 15:54:10 -0700

    style change

--- restaurant_app/lib/RestaurantList.dart
+++ restaurant_app/lib/RestaurantList.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_app/Restaurant.dart';
-import 'package:restaurant_app/RestaurantPage.dart';
+import 'package:restaurant_app/RestaurantListItem.dart';
 //adapted from https://hackernoon.com/flutter-iii-lists-and-items-6bfa7348ab1b
 
 class RestaurantList extends StatelessWidget {
@@ -25,57 +25,5 @@ class RestaurantList extends StatelessWidget {
 
 }
 
-class RestaurantListItem extends Card {
-  RestaurantListItem(Restaurant restaurant, double scaleFactor, BuildContext context):
-    super(
-        margin: EdgeInsets.only(top: 20.0 * scaleFactor, bottom: 20.0 * scaleFactor, left: 25.0 * scaleFactor, right: 25.0 * scaleFactor),
-        child: new Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            ListTile(
-              onTap: () {
-                Navigator.push(context, new MyCustomRoute(
-                  builder: (BuildContext context) => new RestaurantPage(restaurant)),);
-                },
-              contentPadding: EdgeInsets.symmetric(vertical: 55.0 * scaleFactor, horizontal: 60.0 * scaleFactor),
-              leading: const Icon(Icons.album),
-              title:  new Text(
-                  restaurant.name,
-                  textAlign: TextAlign.left,
-                  style: new TextStyle(
-                    color: Colors.white,
-                    fontSize: 65.0 * scaleFactor,
-                  )
-                ),
-              subtitle: new Text(
-                restaurant.address,
-                textAlign: TextAlign.left,
-                style: new TextStyle(
-                    color: Colors.white,
-                    fontSize: 40.0 * scaleFactor,
-                  )
-                ),
-            ),
-          ],
-        ),
-    );
 
-}
 
-class MyCustomRoute<T> extends MaterialPageRoute<T> {
-  //@src https://stackoverflow.com/questions/43680902/replace-initial-route-in-materialapp-without-animation/43685697#43685697
-  MyCustomRoute({ WidgetBuilder builder, RouteSettings settings })
-      : super(builder: builder, settings: settings);
-
-  @override
-  Widget buildTransitions(BuildContext context,
-      Animation<double> animation,
-      Animation<double> secondaryAnimation,
-      Widget child) {
-    if (settings.isInitialRoute)
-      return child;
-    // Fades between routes. (If you don't want any animation, 
-    // just return child.)
-    return new FadeTransition(opacity: animation, child: child);
-  }
-}
